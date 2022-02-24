2e14c1e10d7e9bed74f3cd9ddddcc47020450546
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-02-24 13:25:03 -0800

    fourchar/idioms: restructure context managers

--- fourchar/fourchar_idioms.py
+++ fourchar/fourchar_idioms.py
@@ -2,27 +2,26 @@ from json import dump, load
 from pinyintools import tokenize_phrase
 from urllib.request import urlopen
 
-# URLs for the idiom dataset
+# URL for the idiom dataset
 URL = 'https://raw.githubusercontent.com/pwxcoo/chinese-xinhua/master/data/idiom.json'
-# PHRASE_URL = 'https://raw.githubusercontent.com/pwxcoo/chinese-xinhua/master/data/ci.json'
 
-# Export options
+# export path
 FILENAME = './idioms.json'
 
 
 def main() -> None:
     with urlopen(URL) as fp:
-        with open(FILENAME, 'w', encoding='utf-8') as fp2:
-            idioms = {}
-            for i in load(fp):
-                if '，' in i['word']:
-                    for j in zip(i['word'].split('，'), i['pinyin'].split('，')):
-                        if len(j[0]) == 4:
-                            idioms[j[0]] = tokenize_phrase(j[1].strip(' ').split(' '))
-                else:
-                    if len(i['word']) == 4:
-                        idioms[i['word']] = tokenize_phrase(i['pinyin'].split(' '))
-            dump(idioms, fp2, ensure_ascii=False)
+        idioms = {}
+        for i in load(fp):
+            if '，' in i['word']:
+                for j in zip(i['word'].split('，'), i['pinyin'].split('，')):
+                    if len(j[0]) == 4:
+                        idioms[j[0]] = tokenize_phrase(j[1].strip(' ').split(' '))
+            else:
+                if len(i['word']) == 4:
+                    idioms[i['word']] = tokenize_phrase(i['pinyin'].split(' '))
+    with open(FILENAME, 'w', encoding='utf-8') as fp:
+        dump(idioms, fp, ensure_ascii=False)
 
 
 if __name__ == '__main__':
