183e4fc7bde5d0c8d8bf1fc7d229b122fb2e43f1
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-02-23 19:09:30 -0800

    pinyintools/pytok: modularize tokenization code

---
+++ pinyintools/__init__.py
@@ -0,0 +1,33 @@
+# Mandarin finals, sorted in descending order of length and ascending alphabetical order within the same length
+__FINALS = [
+    'iang', 'iong',
+    'ang', 'eng', 'ian', 'iao', 'ing', 'ong', 'uai', 'uan',
+    'ai', 'an', 'ao', 'ei', 'en', 'er', 'ia', 'ie', 'in', 'iu', 'ou', 'ua', 'ui', 'un', 'uo', 'üe',
+    'a', 'e', 'i', 'o', 'u', 'ü'
+]
+
+# Mandarin vowel data, mapped to the raw vowel and the tone number
+__TONED_VOWELS = {
+    'ā': ('a', 1), 'á': ('a', 2), 'ǎ': ('a', 3), 'à': ('a', 4),
+    'ē': ('e', 1), 'é': ('e', 2), 'ě': ('e', 3), 'è': ('e', 4),
+    'ī': ('i', 1), 'í': ('i', 2), 'ǐ': ('i', 3), 'ì': ('i', 4),
+    'ō': ('o', 1), 'ó': ('o', 2), 'ǒ': ('o', 3), 'ò': ('o', 4),
+    'ū': ('u', 1), 'ú': ('u', 2), 'ǔ': ('u', 3), 'ù': ('u', 4),
+    'ǖ': ('ü', 1), 'ǘ': ('ü', 2), 'ǚ': ('ü', 3), 'ǜ': ('ü', 4)
+}
+
+
+def tokenize(pinyin: str) -> tuple[str, str, int] | None:
+    initial = final = ''
+    tone = 0
+    for i in pinyin:
+        if i in __TONED_VOWELS:
+            tone = __TONED_VOWELS[i][1]
+            pinyin = pinyin.replace(i, __TONED_VOWELS[i][0])
+            break
+    for f in __FINALS:
+        if f in pinyin:
+            final = f
+            initial = pinyin.rstrip(f)
+            break
+    return (initial, final, tone) if final else None
--- pytok/pytok.py
+++ pytok/pytok.py
@@ -1,25 +1,8 @@
 from json import dump
 from types import NoneType
 from typing import Dict, List, Tuple, Union
+from pinyintools import tokenize
 from unihan_etl.process import Packager
-
-# Mandarin finals, sorted in descending order of length and ascending alphabetical order within the same length
-FINALS = [
-    'iang', 'iong',
-    'ang', 'eng', 'ian', 'iao', 'ing', 'ong', 'uai', 'uan',
-    'ai', 'an', 'ao', 'ei', 'en', 'er', 'ia', 'ie', 'in', 'iu', 'ou', 'ua', 'ui', 'un', 'uo', 'üe',
-    'a', 'e', 'i', 'o', 'u', 'ü'
-]
-
-# Mandarin vowel data, mapped to the raw vowel and the tone number
-TONED_VOWELS = {
-    'ā': ('a', 1), 'á': ('a', 2), 'ǎ': ('a', 3), 'à': ('a', 4),
-    'ē': ('e', 1), 'é': ('e', 2), 'ě': ('e', 3), 'è': ('e', 4),
-    'ī': ('i', 1), 'í': ('i', 2), 'ǐ': ('i', 3), 'ì': ('i', 4),
-    'ō': ('o', 1), 'ó': ('o', 2), 'ǒ': ('o', 3), 'ò': ('o', 4),
-    'ū': ('u', 1), 'ú': ('u', 2), 'ǔ': ('u', 3), 'ù': ('u', 4),
-    'ǖ': ('ü', 1), 'ǘ': ('ü', 2), 'ǚ': ('ü', 3), 'ǜ': ('ü', 4)
-}
 
 # import/export options
 FILENAME = './pytok.json'
@@ -30,22 +13,6 @@ OPTIONS = {
     'format': 'python',
     'log_level': 'WARNING'
 }
-
-
-def tokenize(pinyin: str) -> Union[Tuple[str, str, int] | NoneType]:
-    initial = final = ''
-    tone = 0
-    for i in pinyin:
-        if i in TONED_VOWELS:
-            tone = TONED_VOWELS[i][1]
-            pinyin = pinyin.replace(i, TONED_VOWELS[i][0])
-            break
-    for f in FINALS:
-        if f in pinyin:
-            final = f
-            initial = pinyin.rstrip(f)
-            break
-    return (initial, final, tone) if final else None
 
 
 def process(char: dict) -> List[Dict[str, Union[str | Union[Tuple[str, str, int] | NoneType]]]]:
