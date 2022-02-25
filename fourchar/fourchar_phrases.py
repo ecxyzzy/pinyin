f1fac573e6479fa03e8fd753a2a0e025bd842044
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-02-25 11:17:36 -0800

    fourchar/phrases: add non-interactive mode

--- fourchar/fourchar_phrases.py
+++ fourchar/fourchar_phrases.py
@@ -4,14 +4,16 @@ from urllib.request import urlopen
 # URL for the phrase dataset
 URL = 'https://raw.githubusercontent.com/pwxcoo/chinese-xinhua/master/data/ci.json'
 
+# configuration
 # local hanzi dataset path
 HANZI = './hanzi.json'
-
 # export path
 PHRASE = './phrases.json'
+# whether to prompt for entries with multiple readings
+INTERACTIVE = False
 
 
-def prompt_reading(phrase: str, char: str, readings: list[tuple[str, str, int] | None]) -> tuple[str, str, int] | None:
+def prompt_reading(phrase: str, char: str, readings: list[tuple[str, str, int]]) -> tuple[str, str, int]:
     print(f'Character "{char}" in phrase "{phrase}" has multiple recording readings:')
     [print(f'[{i}]: {readings[i]})') for i in range(len(readings))]
     while True:
@@ -22,13 +24,16 @@ def prompt_reading(phrase: str, char: str, readings: list[tuple[str, str, int] |
             print('Invalid input.')
 
 
-def process(db: dict[str, list[tuple[str, str, int] | None]], phrase: str) -> list[tuple[str, str, int] | None]:
+def process(db: dict[str, list[tuple[str, str, int]]], phrase: str) -> list[tuple[str, str, int]]:
     ret = []
     for i in phrase:
-        if len(db[i]) == 1:
-            ret.append(db[i][0])
+        if i in db:
+            if len(db[i]) == 1:
+                ret.append(db[i][0])
+            else:
+                ret.append(prompt_reading(phrase, i, db[i]) if INTERACTIVE else None)
         else:
-            ret.append(prompt_reading(phrase, i, db[i]))
+            ret.append(None)
     return ret
 
 
@@ -41,10 +46,16 @@ def main() -> None:
             if '，' in i['ci']:
                 for j in i['ci'].split('，'):
                     if len(j) == 4:
-                        phrases[j] = process(hanzi, j)
+                        p = process(hanzi, j)
+                        if any(i is None for i in p):
+                            continue
+                        phrases[j] = p
             else:
                 if len(i['ci']) == 4:
-                    phrases[i['ci']] = process(hanzi, i['ci'])
+                    p = process(hanzi, i['ci'])
+                    if any(i is None for i in p):
+                        continue
+                    phrases[i['ci']] = p
     with open(PHRASE, 'w', encoding='utf-8') as fp:
         dump(phrases, fp, ensure_ascii=False)
 
